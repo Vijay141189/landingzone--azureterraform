7c7bce19e26a8e8a4c8546af722c12a47cfaf61f
Vijay <vijay@example.com>
2026-08-03 12:35:11 +0530

    rg name

--- environment/dev/provider.tf
+++ environment/dev/provider.tf
@@ -1,13 +1,20 @@
 terraform {
     required_providers {
         azurerm = {
-            source = "hashicorp/azurerm"
+            source  = "hashicorp/azurerm"
             version = "4.81.0"
         }
         random = {
             source  = "hashicorp/random"
             version = "~> 3.6"
         }
+    }
+
+    backend "azurerm" {
+        resource_group_name  = "terraform-state-rg"
+        storage_account_name = "tfstatestorage1411"
+        container_name        = "tfstate"
+        key                    = "dev.terraform.tfstate"
     }
 }
 
